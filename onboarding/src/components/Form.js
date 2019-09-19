728bbc01b9ef30a9c7bd079f1f5bd6ae02e25f12
Dom <domeccleston@gmail.com>
2019-09-19 16:01:19 +0100

    axios

--- onboarding/src/components/Form.js
+++ onboarding/src/components/Form.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { withFormik, Form, Field } from "formik";
+import axios from 'axios';
 import * as Yup from 'yup';
 
 
-function LoginForm({ errors, touched }) {
+function LoginForm({ values, errors, touched }) {
     return (
-        <form>
+        <Form>
             <div>
             {touched.username && errors.username && <p>{errors.username}</p>} 
             </div>
@@ -30,17 +31,22 @@ function LoginForm({ errors, touched }) {
             name="password"
             placeholder="Password:"
             />
-            <input type="checkbox" name="tos" onChange={event => console.log(event)}></input>
-            <button>Submit!</button>
-        </form>
+            <label>
+                Accept TOS
+            <input type="checkbox" name="tos" checked={values.tos} onChange={(event) => console.log(event)}></input>
+            </label>
+            <button type="submit">Submit!</button>
+        </Form>
     )
 }
 
 const FormikLoginForm = withFormik({
-    mapPropsToValues({ username, password }) {
+    mapPropsToValues({ username, email, password, tos }) {
         return {
             username: username || "",
+            email: email || "",
             password: password || "",
+            tos: tos || false,
         };
     },
 
@@ -57,9 +63,25 @@ const FormikLoginForm = withFormik({
     }),
     //======END VALIDATION SCHEMA======
 
-    handleSubmit(values) {
-        console.log(values);
-    }
+    // SUBMIT POST REQ
+    handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
+        if (values.email === "alreadytaken@atb.dev") {
+          setErrors({ email: "That email is already taken" });
+        } else {
+          axios
+            .post("https://reqres.in/api/users", values)
+            .then(res => {
+              console.log(res); // Data was created successfully and logs to console
+              resetForm();
+              setSubmitting(false);
+            })
+            .catch(err => {
+              console.log(err); // There was an error creating the data and logs to console
+              setSubmitting(false);
+            });
+        }
+      }
+    // END POST
 
 })(LoginForm);
 
