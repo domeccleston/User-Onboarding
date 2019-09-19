3c8687f596b6eaff20a92b5a86ea9801dcbfc3a0
Dom <domeccleston@gmail.com>
2019-09-19 15:51:25 +0100

    added custom error codes

--- onboarding/src/components/Form.js
+++ onboarding/src/components/Form.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { withFormik, Form, Field } from "formik";
+import * as Yup from 'yup';
 
-function LoginForm() {
+
+function LoginForm({ errors, touched }) {
     return (
         <form>
+            <div>
+            {touched.username && errors.username && <p>{errors.username}</p>} 
+            </div>
             <Field
             type="text"
             name="username"
             placeholder="Name:"
             />
+            <div>
+                {touched.email && errors.email && <p>{errors.email}</p>}    
+            </div>
             <Field
             type="email"
             name="email"
             placeholder="Email:"
             />
+            <div>
+                {touched.password && errors.password && <p>{errors.password}</p>}
+            </div>
             <Field
             type="password"
             name="password"
@@ -32,6 +43,19 @@ const FormikLoginForm = withFormik({
             password: password || "",
         };
     },
+
+    //======VALIDATION SCHEMA==========
+    validationSchema: Yup.object().shape({
+        username: Yup.string()
+            .required("Error 1: username is required"),
+        email: Yup.string()
+            .email()
+            .required("Error 2: email is required"),
+        password: Yup.string()
+            .min(6)
+            .required("Error 3: password is required")
+    }),
+    //======END VALIDATION SCHEMA======
 
     handleSubmit(values) {
         console.log(values);
